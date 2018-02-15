b12c1993106d443361c9804b5ff7517c729b4750
Damien's GitHub <dburksgtr@gmail.com>
2018-02-14 21:57:35 -0600

    Adding another program for Crypto.

---
+++ vigerneCipher.py
@@ -0,0 +1,15 @@
+#Going to help with encoding and decoding CS 547
+
+def encode():
+    print("Encoding Program for Vigerne Cipher")
+    print("What is the name of the key for this cipher?")
+
+    keyString = input()
+    key = str(keyString)
+
+    ## Construction of the program, double loop or not a loop...
+    
+
+
+if __name__ == '__main__':
+    encode()
